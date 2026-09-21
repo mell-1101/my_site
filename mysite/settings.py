c7abfd0a97412dc0c44cc90f9aa92c27ac781ad3
sage <sage.thoughts430@gmail.com>
2026-09-21 15:18:36 +0330

    bought a template and made the static files urls correct and added a base.html for footer and header

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,9 +116,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=[
-    BASE_DIR / 'static',
-        ]
+
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS=[
+    BASE_DIR /"static",
+    '/var/www/static/',
+]
 
 
 # Email
